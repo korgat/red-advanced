787a8d352abaf230aca95a04f8f2a38d7dffdefc
Taras Gasyuk <tarasgasyuk@gmail.com>
2025-02-09 16:56:37 +0100

    make sidebar common layout (prevent heder content blinking)

--- src/app/(public)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '@/components/layout/Layout'
-
-export default function PublicLayout({
-	children
-}: Readonly<{
-	children: React.ReactNode
-}>) {
-	return <Layout>{children}</Layout>
-}
--- src/app/my/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '@/components/layout/Layout'
-
-export default function UserLayout({
-	children
-}: Readonly<{
-	children: React.ReactNode
-}>) {
-	return <Layout>{children}</Layout>
-}
