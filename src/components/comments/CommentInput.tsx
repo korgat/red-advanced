bd16d0f431ae5bb32254dfbb3338a04f794ffd4b
Taras Gasyuk <tarasgasyuk@gmail.com>
2025-03-19 17:12:43 +0100

    change prettier settings

--- src/components/comments/CommentInput.tsx
+++ src/components/comments/CommentInput.tsx
@@ -1,43 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-import Button from '@/ui/button/Button'
-import { Textarea } from '@/ui/field'
+import Button from '@/ui/button/Button';
+import { Textarea } from '@/ui/field';
 
-import { useCommentsForm } from './useCommentForm'
-import { cn } from '@/lib/utils'
+import { useCommentsForm } from './useCommentForm';
+import { cn } from '@/lib/utils';
 
 interface CommentInputProps extends React.HTMLAttributes<HTMLFormElement> {
-	videoId: string
-	publicId: string
+  videoId: string;
+  publicId: string;
 }
 
 const CommentInput = (props: CommentInputProps) => {
-	const { className = '', videoId, publicId, ...rest } = props
+  const { className = '', videoId, publicId, ...rest } = props;
 
-	const { errors, isLoading, onSubmit, register } = useCommentsForm(videoId, publicId)
+  const { errors, isLoading, onSubmit, register } = useCommentsForm(videoId, publicId);
 
-	return (
-		<form
-			{...rest}
-			className={cn('flex gap-16', {}, [className])}
-			onSubmit={onSubmit}
-		>
-			<Textarea
-				className='mb-0 w-full'
-				registration={register('comment', { required: 'Cant be empty' })}
-				errorMessage={errors.comment?.message}
-				placeholder='Enter comment:'
-				rows={1}
-			/>
-			<Button
-				disabled={isLoading}
-				variant='secondary'
-				type='submit'
-			>
-				Comment
-			</Button>
-		</form>
-	)
-}
+  return (
+    <form
+      {...rest}
+      className={cn('flex gap-16', {}, [className])}
+      onSubmit={onSubmit}
+    >
+      <Textarea
+        className='mb-0 w-full'
+        registration={register('comment', { required: 'Cant be empty' })}
+        errorMessage={errors.comment?.message}
+        placeholder='Enter comment:'
+        rows={1}
+      />
+      <Button
+        disabled={isLoading}
+        variant='secondary'
+        type='submit'
+      >
+        Comment
+      </Button>
+    </form>
+  );
+};
 
-export default CommentInput
+export default CommentInput;
